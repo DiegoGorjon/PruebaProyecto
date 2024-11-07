8d9fccdca87be284eae3d580dad057690104369c
Diego Gorjon <ikdhc@plaiaundi.net>
2024-11-07 10:18:19 +0100

    Leer.closeCommit

--- prueba/src/App.java
+++ prueba/src/App.java
@@ -7,7 +7,7 @@ public class App {
         Scanner leer = new Scanner(System.in);
         String nombre = leer.nextLine();
         System.out.println("Hola, "+nombre+"!");
-        
+        leer.close();
         
     }
 }
